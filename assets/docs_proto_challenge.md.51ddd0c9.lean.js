1cab73160247c2b8cbc0a16c05c9a0ec200a80f4
mingyuLi97 <mingyuLi97@users.noreply.github.com>
2023-05-25 07:39:37 +0000

    deploy: 03c09a50a2137dbc51d51a710b196c33cd02584b

---
+++ assets/docs_proto_challenge.md.51ddd0c9.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o,N as a}from"./chunks/framework.57e8d462.js";const d=JSON.parse('{"title":"prototype-challenge","description":"","frontmatter":{},"headers":[],"relativePath":"docs/proto/challenge.md"}'),p={name:"docs/proto/challenge.md"},l=a("",7),e=[l];function t(c,B,r,y,F,A){return o(),n("div",null,e)}const C=s(p,[["render",t]]);export{d as __pageData,C as default};
